cba6b37541247ea5972acb01a82c984bd428eaf0
doniyb <doniyb@yandex.ru>
2024-07-29 17:12:51 +0300

    Прототип

--- MyForm.cpp
+++ MyForm.cpp
@@ -1,7 +1,13 @@
 #include "MyForm.h"
+#include <string>
+#include <vector>
+#include <ctime>
+#include <msclr\marshal_cppstd.h>
+
 
 using namespace System;
 using namespace System::Windows::Forms;
+
 
 int main(array<String^>^ args_main)
 {
@@ -11,3 +17,66 @@ int main(array<String^>^ args_main)
 	Application::Run(% form);
 	return 0;
 }
+
+Tetsttypes::MyForm::MyForm(void)
+{
+	InitializeComponent();
+}
+
+std::vector <std::string> words{ "привет", "дом", "рука", "вид", "вопрос", "время", "он", "как", 
+"его", "год", "голова", "дело", "день", "друг", "жизнь", "конец", "лицо", "место", 
+"мир", "работа", "раз", "ребенок", "сила", "слово", "случай", "сторона", "страна", "человек", 
+"быть", "взять", "видеть", "говорить", "дать", "думать", "жить", "знать", "идти" , "иметь" ,
+"мочь" , "оказаться" 
+, "получить" , "понять" , "работать" , "сделать" , "сидеть" , "сказать" , "смотреть" , "спросить" 
+, "стать" , "хотеть" , "больше" , "вдруг", "вмесете", "вообще", "еще", "меньше", "особенно", "очень", 
+"почти", "снова", "совсем", "сразу", "уже", "хорошо", "белый"
+, "большой" , "высокий" , "главный" , "государственный" , "далекий" , "маленький" , "молодой" 
+, "настоящий" , "новый" , "нужный" , "общий" , "основной", "полный", "последний"
+, "разный" , "российский" , "собственный" , "старый" , "черный" , "где" , "свой" ,
+"я" , "ты" , "этот" , "такой" , "свой", "она", "они"
+, "себя" , "бежать" , "продовать" , "водить" , "петь", "пить", "есть" };
+int num_word = 0;
+int typech = 0;
+
+System::Void Tetsttypes::MyForm::textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+}
+
+
+System::Void Tetsttypes::MyForm::textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+
+
+
+	msclr::interop::marshal_context context;
+	std::string str1 = context.marshal_as<std::string>(textInput->Text);
+	std::string str3 = context.marshal_as<std::string>(textOutput->Text);
+
+
+	for (int i = 0; i < str1.size(); ++i) {
+		if (str1[i] != str3[i]) {
+			str1.erase(str1.begin() + i);
+			textInput->Text = gcnew String(str1.c_str());
+		}
+	}
+
+	if (textInput->Text == textOutput->Text) {
+		typech++;
+		srand(time(0));
+		num_word = rand() % words.size();
+		textOutput->Text = gcnew String(words[num_word].c_str());
+		textInput->Text = "";
+	}
+	typed->Text = "Набранные: " + gcnew String(std::to_string(typech).c_str());
+}
+
+
+System::Void Tetsttypes::MyForm::label1_Click(System::Object^ sender, System::EventArgs^ e)
+{
+}
+
+
+
+
+
--- MyForm.h
+++ MyForm.h
@@ -9,19 +9,11 @@ namespace Tetsttypes {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Сводка для MyForm
-	/// </summary>
+
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
-		MyForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
-		}
+		MyForm(void);
 
 	protected:
 		/// <summary>
@@ -34,6 +26,16 @@ namespace Tetsttypes {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textInput;
+	private: System::Windows::Forms::TextBox^ textOutput;
+	private: System::Windows::Forms::Label^ typed;
+	protected:
+
+
+
+
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,21 +50,68 @@ namespace Tetsttypes {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->textInput = (gcnew System::Windows::Forms::TextBox());
+			this->textOutput = (gcnew System::Windows::Forms::TextBox());
+			this->typed = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// textInput
+			// 
+			this->textInput->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textInput->Location = System::Drawing::Point(71, 192);
+			this->textInput->Name = L"textInput";
+			this->textInput->Size = System::Drawing::Size(243, 35);
+			this->textInput->TabIndex = 0;
+			this->textInput->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textOutput
+			// 
+			this->textOutput->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textOutput->Location = System::Drawing::Point(71, 68);
+			this->textOutput->Name = L"textOutput";
+			this->textOutput->ReadOnly = true;
+			this->textOutput->Size = System::Drawing::Size(243, 35);
+			this->textOutput->TabIndex = 2;
+			this->textOutput->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox3_TextChanged);
+			// 
+			// typed
+			// 
+			this->typed->AutoSize = true;
+			this->typed->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->typed->Location = System::Drawing::Point(182, 251);
+			this->typed->Name = L"typed";
+			this->typed->Size = System::Drawing::Size(132, 25);
+			this->typed->TabIndex = 3;
+			this->typed->Text = L"Набранные:";
+			this->typed->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(384, 461);
+			this->Controls->Add(this->typed);
+			this->Controls->Add(this->textOutput);
+			this->Controls->Add(this->textInput);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Speed Print";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e);
 	};
+
+
+
 }
+
